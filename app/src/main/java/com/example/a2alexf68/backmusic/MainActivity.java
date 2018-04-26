412e475933ea5725a931bdd1a274ff819e1d607c
2alexf68 <2alexf68@gmail.com>
2018-04-26 15:27:49 +0100

    Initial commit v 0.1

---
+++ app/src/main/java/com/example/a2alexf68/backmusic/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.a2alexf68.backmusic;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    Intent startIntent = new Intent(this, MusicService.class);
+
+    @Override
+    public ComponentName startService(Intent service) {
+        startService(startIntent);
+    }
+}
---
+++ app/src/main/java/com/example/a2alexf68/backmusic/MusicService.java
@@ -0,0 +1,24 @@
+package com.example.a2alexf68.backmusic;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+/**
+ * Created by 2alexf68 on 26/04/2018.
+ */
+
+public class MusicService extends Service {
+
+    class MusicServiceBinder extends Binder
+    {
+        public MusicService getService()
+        {
+            return MusicService.this;
+        }
+        public IBinder onBind(Intent intent){
+            return new MusicServiceBinder();
+        }
+    }
+}
